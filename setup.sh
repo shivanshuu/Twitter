3707e208d9c5ab8b3c562b11af1c506025df763c
Shiv <shivanshuu@hotmail.com>
2018-07-12 13:26:37 +0000

    Changes to be committed: 	new file:   setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#sudo apt-get update
+#sudo apt-get upgrade
+#sudo apt-get install python
+#sudo apt-get install python-setuptools python-dev build-essential
+#sudo easy_install pip
+sudo pip install pip-requirements.txt --user twitter
+
